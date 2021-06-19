721c5b4fc720eca6563e1ce72da4dc4f29697762
Filipe Dantas <filipedantas90@hotmail.com>
2021-06-19 13:46:33 -0300

    avatar change

--- src/pages/Profile/styles.ts
+++ src/pages/Profile/styles.ts
@@ -55,29 +55,39 @@ export const AvatarInput = styled.div`
   margin-bottom: 32px;
   position: relative;
   align-self: center;
+
   img {
     width: 186px;
     height: 186px;
     border-radius: 50%;
   }
-  button {
+
+  label {
     position: absolute;
     width: 48px;
     height: 48px;
-    background-color: #ff9000;
+    background: #ff9000;
     border-radius: 50%;
     right: 0;
     bottom: 0;
     border: 0;
+    cursor: pointer;
     transition: background-color 0.2s;
+
     display: flex;
     align-items: center;
     justify-content: center;
+
+    input {
+      display: none;
+    }
+
     svg {
       width: 20px;
       height: 20px;
       color: #312e38;
     }
+
     &:hover {
       background: ${shade(0.2, '#ff9000')};
     }
